a20d5a95c5bbb2540193104c392e8989a443219e
DarkIgnite <ryanfadhila18@gmail.com>
2026-05-07 13:51:15 +0800

    feat: add gallery page and update navigation menu links in index.php

--- gallery.php
+++ gallery.php
@@ -38,40 +38,8 @@ $a = mysqli_fetch_object($kontak);
         </div>
     </div>
 
-    <!-- search -->
-    <div class="search">
-        <div class="container">
-            <p>Silahkan login untuk berbelanja, </p>
-            <a href="login.php"><strong>Klik disini</strong></a>
-        </div>
-    </div>
 
     <!-- category -->
-    <div class="section">
-        <div class="container">
-            <h3>Kategori</h3>
-            <div class="box">
-                <?php
-                $kategori = mysqli_query($conn, "SELECT * FROM tb_category ORDER BY category_id DESC");
-
-                if(mysqli_num_rows($kategori) > 0){
-                    while($k = mysqli_fetch_array($kategori)){
-                ?>
-                        <a href="index.php?kat=<?php echo $k['category_id'] ?>">
-                            <div class="col-5">
-                                <img src="img/food.png" width="50px" style="margin-bottom:5px;">
-                                <p><?php echo $k['category_name'] ?></p>
-                            </div>
-                        </a>
-                <?php
-                    }
-                } else {
-                ?>
-                    <p>Kategori tidak ada</p>
-                <?php } ?>
-            </div>
-        </div>
-    </div>
 
     <!-- new product -->
     <div class="section">
